f96362fd091b004a1d867edded06ad362558d4d8
Daiani <Daianim3@Daianis-MacBook-Air.local>
2015-03-31 16:54:29 -0400

    numbers

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,6 @@
 puts "what is your name?"
+name= gets.chomp
+puts "Hi #{name}!"
+puts "How old are you?"
+old= gets.chomp
+puts "2015 #{old}"
---
+++ exercise4.rb
@@ -0,0 +1,13 @@
+count = (1..100)
+
+count.each do |x|
+	if  x.remainder(3) == 0
+		puts "Bit" 
+	elsif x.remainder(5) == 0
+		puts "Maker"
+	elsif x.remainder(15) == 0
+		puts "BitMakers"
+	else 
+		puts "#{x}"
+	end
+end
